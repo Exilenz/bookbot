e7b4ac69bc80b9175818e30cd7d10e60861b8b66
Exilenz <vaclavek.ales@gmail.com>
2024-12-16 14:54:42 +0100

    Readfile done

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def count_words():
+    words = get_book_text.split()
+    print(words + 1)
